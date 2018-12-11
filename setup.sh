7caac68ab61382b0583bb5ff8372bb866d34e5ff
FAlkhanaizi <fatema.alkhanaizi@durham.ac.uk>
2018-12-11 23:17:16 +0000

    removed exclusive

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 module load pgi/17.9
-
+module load slurm
 module load intel/xe_2018.2
 source /ddn/apps/Cluster-Apps/intel/xe_2018.2/vtune_amplifier/amplxe-vars.sh
 source /ddn/apps/Cluster-Apps/intel/xe_2018.2/advisor/advixe-vars.sh
